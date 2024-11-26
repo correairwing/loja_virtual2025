f747f24b7116f2ca1395fec27b0321f778ebdc38
correairwing <irwingcorrea95@gmail.com>
2024-11-26 11:52:04 -0400

    importação inicial do projeto

---
+++ webstore/src/main/java/com/icorrea/webstore/LojaVirtual2025Application.java
@@ -0,0 +1,13 @@
+package com.icorrea.webstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaVirtual2025Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaVirtual2025Application.class, args);
+	}
+
+}
