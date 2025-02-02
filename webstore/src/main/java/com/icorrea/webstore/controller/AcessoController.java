0b2b7effe974efa03387c4d91d13849eafd8c87f
Irwing Correa <irwingcorrea.dev@gmail.com>
2025-02-02 15:50:25 -0400

    adicionando configuração de segurança, ajustando controlador de acesso e incluindo dependência do JUnit

--- webstore/src/main/java/com/icorrea/webstore/controller/AcessoController.java
+++ webstore/src/main/java/com/icorrea/webstore/controller/AcessoController.java
@@ -1,8 +1,12 @@
 package com.icorrea.webstore.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.icorrea.webstore.model.Acesso;
 import com.icorrea.webstore.service.AcessoService;
@@ -13,9 +17,11 @@ public class AcessoController {
     @Autowired
     private AcessoService acessoService;
 
-    @PostMapping("/salvarAcesso")
-    public Acesso salvarAcesso(Acesso acesso) {
-        return acessoService.save(acesso);
+    @ResponseBody
+    @PostMapping(value = "**/salvarAcesso")
+    public ResponseEntity<Acesso> salvarAcesso(@RequestBody Acesso acesso) {
+        Acesso acessoSalvo = acessoService.save(acesso);
+        return new ResponseEntity<Acesso>(acessoSalvo, HttpStatus.OK);
     }
 
 }
