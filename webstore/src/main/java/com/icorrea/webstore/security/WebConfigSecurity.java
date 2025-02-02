f5982f41524515fd5c74229d6a8eee499c15c64b
Irwing Correa <irwingcorrea.dev@gmail.com>
2025-02-02 15:59:12 -0400

    atualizando dependências do projeto para versões mais antigas e ajustando imports de Jakarta para javax nas entidades

--- webstore/src/main/java/com/icorrea/webstore/security/WebConfigSecurity.java
+++ webstore/src/main/java/com/icorrea/webstore/security/WebConfigSecurity.java
@@ -1,30 +1,13 @@
 package com.icorrea.webstore.security;
 
-import java.beans.Customizer;
-
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
 public class WebConfigSecurity{
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-            .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/publico/**").permitAll()
-                .anyRequest().authenticated()
-            )
-            .formLogin(login -> login.defaultSuccessUrl("/home", true)) // Definir redirecionamento após login
-
-            .logout(logout -> logout.logoutSuccessUrl("/login?logout")); // Configurar logout
-
-        return http.build();
-    }
+    
 }
 
--- webstore/src/test/java/com/icorrea/webstore/LojaVirtual2025ApplicationTests.java
+++ webstore/src/test/java/com/icorrea/webstore/LojaVirtual2025ApplicationTests.java
@@ -6,17 +6,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import com.icorrea.webstore.controller.AcessoController;
 import com.icorrea.webstore.model.Acesso;
-import com.icorrea.webstore.repository.AcessoRepository;
-import com.icorrea.webstore.service.AcessoService;
 
 @SpringBootTest(classes = LojaVirtual2025Application.class )
 class LojaVirtual2025ApplicationTests {
-
-	@Autowired
-	private AcessoService acessoService;
-
-	@Autowired
-	private AcessoRepository acessoRepository;
 
 	@Autowired
 	private AcessoController acessoController;
